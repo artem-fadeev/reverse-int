20028205f93ffafbfa349f08a3af77b31fc8b3cb
Artme Fadeev <fau1992@mail.ru>
2020-02-21 12:27:14 +0300

    task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let str = ('' + n).split('');
+    let result = [];
+    for(let i = str.length - 1; i >= 0 ; i--) {
+      result.push(str[i]);
+    }
+    return parseInt(result.join(''));
+    
+    // return parseInt((n+'').split('').reverse().join('')); - with reverse() function
 }
